b954824f1d0150a8124d77a22e4ad2d57809b258
Arian <arian.kulmer@web.de>
2020-04-24 14:27:51 +0200

    raphael: init: Workaround safetynet
    Change-Id: I03ce5363ade91feaa33df52fda7f9942fe50898a

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -118,11 +118,12 @@ void vendor_load_properties() {
         mod_device = "raphaelin_in_global";
     }
 
-    property_override("ro.apex.updatable", "false");
-    
     // SafetyNet workaround
     property_override("ro.boot.verifiedbootstate", "green");
+    fingerprint = "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys";
+    description = "dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys";
 
+    set_ro_build_prop("fingerprint", fingerprint);
     set_ro_product_prop("device", device);
     set_ro_product_prop("model", model);
     property_override("ro.build.description", description.c_str());
