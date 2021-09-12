3478a5700db3b4712d4ddb2bfa621cc2c494fcf1
Arian <arian.kulmer@web.de>
2021-09-12 19:37:27 +0200

    TP1803: rootdir: Move remaining write from qcom post boot script to init.target.rc
    Change-Id: I4a8b4e3922433d9276617bed6177ce3ae7271ada

--- device.mk
+++ device.mk
@@ -272,7 +272,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.power.rc \
-    init.qcom.post_boot.sh \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom.usb.rc \
