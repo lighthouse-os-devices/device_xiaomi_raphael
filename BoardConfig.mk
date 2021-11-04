c91dc2103a281cdd5909d30cf0fedb1ccc57bba7
CannedShroud <hypersync112@gmail.com>
2021-11-04 23:39:46 +0530

    raphael-sepeolicy: Import pixelstats sepolicy
    Signed-off-by: CannedShroud <hypersync112@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -172,6 +172,7 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/google
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
